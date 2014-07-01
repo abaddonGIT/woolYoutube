8b0a683b5918b9dd8c0b7e2d4cb40f9cb6439fa3
abaddonGIT <abaddongit@gmail.com>
2014-07-01 15:05:29 +0400

    doc

--- README.md
+++ README.md
@@ -1,4 +1,61 @@
 woolYoutube
 ===========
 
-Плагин для поиска видео на youtube
+jQuery плагин для поиска и вывода видео на youtube.
+
+<h3>Параметры плагина:</h3>
+<ul>
+    <li><b>max-results</b> - сколько выводить</li>
+    <li><b>start-index</b> - номер стартовой позиции</li>
+    <li><b>caption</b> - искать видео, только с субтитрами</li>
+    <li><b>category</b> - категория</li>
+    <li><b>lr</b> - язык видео по стандарту ISO 639-1</li>
+    <li><b>orderby</b> - сортировка по полям relevance, published, viewCount и rating</li>
+</ul>
+<h3>Как использовать?</h3>
+<pre>
+woolYoutube.search(string, function (videos) {
+    var ln = videos.length;
+    result.html('');
+    if (videos.length) {
+        for(var i = 0; i < ln; i++) {
+            result.append('<div class="trailer">' +
+                        '<iframe width="640" height="360" src="http://www.youtube.com/embed/' + videos[i].id + '" frameborder="0" allowfullscreen></iframe>' +
+                        '</div>');
+            }
+        } else {
+            alert("По вашему запросу не чего не было найдено!");
+        }
+});
+</pre>
+<hr />
+woolYoutube
+===========
+
+jQuery plugin for search and display video from youtube.
+
+<h3>Plugin settings:</h3>
+<ul>
+    <li><b>max-results</b> - how many to show</li>
+    <li><b>start-index</b> - number of a launching position</li>
+    <li><b>caption</b> - to look for video, only with subtitlings</li>
+    <li><b>category</b> - category</li>
+    <li><b>lr</b> - video language according to the standard ISO 639-1</li>
+    <li><b>orderby</b> - sorting according to fields relevance, published, viewCount и rating</li>
+</ul>
+<h3>How to use it?</h3>
+<pre>
+woolYoutube.search(string, function (videos) {
+    var ln = videos.length;
+    result.html('');
+    if (videos.length) {
+        for(var i = 0; i < ln; i++) {
+            result.append('<div class="trailer">' +
+                        '<iframe width="640" height="360" src="http://www.youtube.com/embed/' + videos[i].id + '" frameborder="0" allowfullscreen></iframe>' +
+                        '</div>');
+            }
+        } else {
+            alert("No results!");
+        }
+});
+</pre>
--- js/woolYoutube.js
+++ js/woolYoutube.js
@@ -4,131 +4,126 @@
  * @version 0.0.1
  * ***************************************************/
 
- var woolYoutube = (function () {
- 	var d = document, 
- 		w = window,
- 		$D = $(d),
- 		$W = $(w),
- 		config = {},
- 		videos = [],
- 		baseUrl = 'http://gdata.youtube.com/feeds/api/videos?alt=json',//адрес для запроса
- 		wool = function () {},
- 		proto = wool.prototype,
- 		//распаршивает ответ и возвращает объект
- 		parseResult = function (entries) {
- 			var result = {},
- 				id = entries.id.$t.split('/'), 
- 				ln = id.length;
- 			
- 			result = {
- 				'id': id[ln-1],
- 				'title': entries.title.$t,
- 				'thumbs': entries.media$group.media$thumbnail,
- 				'video': 'http://www.youtube.com/watch?v=' + id[ln-1],
- 				'duration': entries.media$group.yt$duration.seconds
- 			};
+var woolYoutube = (function () {
+    var d = document,
+        w = window,
+        $D = $(d),
+        $W = $(w),
+        config = {},
+        videos = [],
+        baseUrl = 'http://gdata.youtube.com/feeds/api/videos?alt=json',//адрес для запроса
+        wool = function () {
+        },
+        proto = wool.prototype,
+    //распаршивает ответ и возвращает объект
+        parseResult = function (entries) {
+            var result = {},
+                id = entries.id.$t.split('/'),
+                ln = id.length;
 
- 			return result;
- 		};
- 	/*
- 	* Ф-я инициализации плагина
- 	*/	
- 	proto.init = function (options) {
- 		//дефоотовые настройки
- 		config = {
- 			'key': null,//ключ разработчика 
- 			'max-results': 5,//кол-во результатов
- 			'start-index': 0,//Стартовый индекс
- 			'caption': null,//видео только с сабами
- 			'category': null,//категория
- 			'lr': null,//коды языков по стандарту ISO 639-1
- 			'orderby': null//relevance, published, viewCount и rating
- 		};
+            result = {
+                'id': id[ln - 1],
+                'title': entries.title.$t,
+                'thumbs': entries.media$group.media$thumbnail,
+                'video': 'http://www.youtube.com/watch?v=' + id[ln - 1],
+                'duration': entries.media$group.yt$duration.seconds
+            };
 
- 		$.extend(config, options);
- 	};
- 	/*
- 	* делает запрос для получения видео
- 	* @param {String} строка запроса
- 	*/
- 	proto.search = function (q, callback) {
- 		var string = '', url = '';
+            return result;
+        };
+    /*
+     * Ф-я инициализации плагина
+     */
+    proto.init = function (options) {
+        //дефоотовые настройки
+        config = {
+            'max-results': 5,//кол-во результатов
+            'start-index': 0,//Стартовый индекс
+            'caption': null,//видео только с сабами
+            'category': null,//категория
+            'lr': null,//коды языков по стандарту ISO 639-1
+            'orderby': null//relevance, published, viewCount и rating
+        };
 
- 		videos = [],
+        $.extend(config, options);
+    };
+    /*
+     * делает запрос для получения видео
+     * @param {String} строка запроса
+     */
+    proto.search = function (q, callback) {
+        var string = '', url = '';
+        videos = [];
+        if (!q || q === undefined) {
+            throw ('Запрос пуст!');
+        } else {
+            string = decodeURI(q);
+            //строим строку запроса
+            url = baseUrl + this._buildingUrl(string);
 
- 		if (!config.key) {
- 			throw ('Не указан ключ разработчика!!!');
- 		} else {
- 			if (!q || q === undefined) {
- 				throw ('Запрос пуст!');
- 			} else {
- 				string = decodeURI(q);
- 				//строим строку запроса
- 				url = baseUrl + this._buildingUrl(string);
- 				
- 				//делаем запрос
- 				this._ajax({'url': url}, function (data) {
-					if (data.feed) {
-						var feed = data.feed,
-							entries = feed.entry,
-							ln = entries.length;
+            //делаем запрос
+            this._ajax({'url': url}, function (data) {
+                if (data.feed) {
+                    var feed = data.feed,
+                        entries = feed.entry,
+                        ln = entries.length;
 
-						//разбиваем объект
-						while (ln--) {
-							var loc = entries[ln];
-							videos.push(parseResult(loc));
-						}
-					} else {
-						videos.push(parseResult(feed.entry));
-					}	
-					callback(videos);
- 				});
- 			}
- 		}
- 	};
+                    //разбиваем объект
+                    while (ln--) {
+                        var loc = entries[ln];
+                        videos.push(parseResult(loc));
+                    }
+                } else {
+                    videos.push(parseResult(feed.entry));
+                }
+                callback(videos);
+            });
+        }
+    };
 
- 	/*
- 	* Строит строку запроса
- 	*/
- 	proto._buildingUrl = function (string) {
- 		var str = '&q=' + string;
- 		for (var i in config) {
- 			if (config[i]) {
- 				str += '&' + i + '=' + config[i];
- 			}
- 		};
+    /*
+     * Строит строку запроса
+     */
+    proto._buildingUrl = function (string) {
+        var str = '&q=' + string;
+        for (var i in config) {
+            if (config[i]) {
+                str += '&' + i + '=' + config[i];
+            }
+        }
+        ;
 
- 		//str = str.substr(0, str.length - 1);
+        //str = str.substr(0, str.length - 1);
 
- 		return str;
- 	};
+        return str;
+    };
 
- 	/*
- 	* Конструирует запрос
- 	* @param {Object} настройки для запроса
- 	* @param {Function} ф-я после запроса
- 	*/
+    /*
+     * Конструирует запрос
+     * @param {Object} настройки для запроса
+     * @param {Function} ф-я после запроса
+     */
 
- 	proto._ajax = function (options, callback) {
- 		if (options === undefined) {
- 			throw ('Не заданны опции для запроса!');
- 		} else {
- 			$.ajax({
- 				type: options.type || 'get',
- 				dataType: 'json',
- 				processData: true,
- 				url: options.url,
- 				data: options.data,
- 				cache: false,
- 				beforeSend: function () {
+    proto._ajax = function (options, callback) {
+        if (options === undefined) {
+            throw ('Не заданны опции для запроса!');
+        } else {
+            $.ajax({
+                type: options.type || 'get',
+                dataType: 'json',
+                processData: true,
+                url: options.url,
+                data: options.data,
+                cache: false,
+                beforeSend: function () {
 
- 				},
- 				success: function (data) {
- 					callback(data);
- 				}
- 			});
- 		}
- 	};
+                },
+                success: function (data) {
+                    callback(data);
+                }
+            });
+        }
+    };
 
- 	return new wool();
- }(jQuery));
+    return new wool();
+}(jQuery));
